5fa617c16d0bf6015481de0d62beffd5f7f9702e
zaki178 <74228332+zaki178@users.noreply.github.com>
2022-02-26 05:11:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,4 +12,19 @@ apt-get install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
-echo "DONE INSTALLING ALL PACKAGE REQUIRE, TINGGAL RUN npm start"
+echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+npm install @adiwajshing/baileys
+npm install @adiwajshing/baileys@3.5.0
+npm install @adiwajshing/baileys@3.5.1
+npm install @adiwajshing/baileys@3.5.2
+npm install @adiwajshing/baileys@3.5.3
+echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+
+#!/usr/bin/bash
+npm audit 
+npm audit fix
+npm fund
+yarn
+npm start
+celar
+node index.js
